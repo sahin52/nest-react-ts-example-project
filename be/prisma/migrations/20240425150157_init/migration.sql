831ac03366898cbe226f5cf211a8600bae4d2841
sahin52 <sahinkasap52@gmail.com>
2024-04-25 18:25:39 +0300

    swagger and prisma packages added, schema added, product and user services are written initially, dtos initialized

---
+++ be/prisma/migrations/20240425150157_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "image" TEXT,
+    "published" BOOLEAN DEFAULT false,
+    "ownerId" INTEGER,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- be/src/app.controller.ts
+++ be/src/app.controller.ts
@@ -1,12 +1,84 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { ProductService } from './product.service';
+import { User as UserModel, Product as ProductModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly productService: ProductService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('product/:id')
+  async getProductById(@Param('id') id: string): Promise<ProductModel> {
+    return this.productService.post({ id: Number(id) });
+  }
+
+  @Get('feed')
+  async getPublishedProducts(): Promise<ProductModel[]> {
+    return this.productService.products({
+      where: { published: true },
+    });
+  }
+
+  @Get('filtered-products/:searchString')
+  async getFilteredProducts(
+    @Param('searchString') searchString: string,
+  ): Promise<ProductModel[]> {
+    return this.productService.products({
+      where: {
+        OR: [
+          {
+            title: { contains: searchString },
+          },
+          {
+            content: { contains: searchString },
+          },
+        ],
+      },
+    });
+  }
+
+  @Post('product')
+  async createDraft(
+    @Body() productData: { title: string; content?: string; authorEmail: string },
+  ): Promise<ProductModel> {
+    const { title, content, authorEmail } = productData;
+    return this.productService.createProduct({
+      title,
+      content,
+      owner: {
+        connect: { email: authorEmail },
+      },
+    });
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Put('publish/:id')
+  async publishProduct(@Param('id') id: string): Promise<ProductModel> {
+    return this.productService.updateProduct({
+      where: { id: Number(id) },
+      data: { published: true },
+    });
+  }
+
+  @Delete('product/:id')
+  async deleteProduct(@Param('id') id: string): Promise<ProductModel> {
+    return this.productService.deleteProduct({ id: Number(id) });
   }
 }
